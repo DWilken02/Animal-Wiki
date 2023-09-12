86a70b1863d25ff3f9c7c82d34ba2f992c2ad695
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-12 12:03:21 -0700

    change JavaScript in practice branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
+function showAnimal() {
+  let turtleInfo = document.getElementById("turtles");
+  console.log(turtleInfo);
+  let dogInfo = document.getElementById("dogs");
+  let monkeyInfo = document.getElementById("monkeys");
+  let eagleInfo = document.getElementById("eagles");
+  let dolphinInfo = document.getElementById("dolphins");
+  let imSorryMessage = document.getElementById("im-sorry");
+
+  const animal = (document.querySelector("input#animal").value).toLowerCase();
+
+  if (animal === "turtle") {
+    turtleInfo.removeAttribute("class");
+  } else if (animal === "dog") {
+    dogInfo.removeAttribute("class");
+  } else if (animal === "monkey") {
+    monkeyInfo.removeAttribute("class");
+  } else if (animal === "eagle") {
+    eagleInfo.removeAttribute("class");
+  } else if (animal === "dolphin") {
+    dolphinInfo.removeAttribute("class");
+  } else {
+    imSorryMessage.removeAttribute("class");
+  }
+}
 window.onload = function() {
   console.log("onLoad");
   const form = document.querySelector("form");
-  form.onsubmit = function(event) {
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("onSubmit");
-    let turtleInfo = document.getElementById("turtles");
-    console.log(turtleInfo);
-    let dogInfo = document.getElementById("dogs");
-    let monkeyInfo = document.getElementById("monkeys");
-    let eagleInfo = document.getElementById("eagles");
-    let dolphinInfo = document.getElementById("dolphins");
-    let imSorryMessage = document.getElementById("im-sorry");
+    showAnimal();
+  }) 
 
-    const animal = (document.querySelector("input#animal").value).toLowerCase();
-
-    if (animal === "turtle") {
-      turtleInfo.removeAttribute("class");
-    } else if (animal === "dog") {
-      dogInfo.removeAttribute("class");
-    } else if (animal === "monkey") {
-      monkeyInfo.removeAttribute("class");
-    } else if (animal === "eagle") {
-      eagleInfo.removeAttribute("class");
-    } else if (animal === "dolphin") {
-      dolphinInfo.removeAttribute("class");
-    } else {
-      imSorryMessage.removeAttribute("class");
-    }
-  };
 }
